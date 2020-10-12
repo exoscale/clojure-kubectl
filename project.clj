803c845ab1e66ef4960d59cd43bfbbe905ecc7a7
mcorbin <corbin.math@gmail.com>
2020-10-12 15:28:58 +0200

    Version 0.1.7

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject exoscale/kubectl "0.1.7-SNAPSHOT"
+(defproject exoscale/kubectl "0.1.7"
   :description "A wrapper around the kubectl CLI"
   :url "https://github.com/exoscale/clojure-kubectl"
   :plugins [[lein-cljfmt "0.7.0"]
