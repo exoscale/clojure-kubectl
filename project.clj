a0707e7d85a12ca5c321a7b4dbfb627f52d09b9d
Max Penet <max.penet@exoscale.ch>
2021-03-17 09:22:51 +0100

    Version 0.1.10

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject exoscale/kubectl "0.1.10-SNAPSHOT"
+(defproject exoscale/kubectl "0.1.10"
   :description "A wrapper around the kubectl CLI"
   :url "https://github.com/exoscale/clojure-kubectl"
   :plugins [[lein-cljfmt "0.7.0"]
